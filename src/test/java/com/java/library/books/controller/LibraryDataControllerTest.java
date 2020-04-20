18b59c35e4edadad922eeb25c7ce0d4956b3d7a7
kaushiknitsil <kaushiknitsil@gmail.com>
2020-04-20 16:46:41 +0530

    Test cases added

--- src/test/java/com/java/library/books/controller/LibraryDataControllerTest.java
+++ src/test/java/com/java/library/books/controller/LibraryDataControllerTest.java
@@ -10,21 +10,13 @@ import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+
 
 
 import java.util.ArrayList;
@@ -34,12 +26,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class LibraryDataControllerTest extends AbstractTest {
 
-    @Autowired
-    public LibraryDataController libraryDataController;
+    @MockBean
+    public LibraryDataController libraryDataControllerMock;
 
     @Mock
     public LibraryRepository libraryRepository;
@@ -93,22 +86,65 @@ class LibraryDataControllerTest extends AbstractTest {
         return library;
     }
 
+    private List<Book> bookList(){
+        Library library = new Library();
+        library.setLibraryName("Bangalore");
+
+
+        Book book1 = new Book();
+        book1.setAuthor("ABV");
+        book1.setIsbn("IDGB453543");
+        book1.setPublisher("Tata McGraaw:: ");
+        book1.setLanguage("English");
+        book1.setLibrary(library);
+        Book book2 = new Book();
+        book2.setAuthor("ABdsfdfV");
+        book2.setIsbn("IDGBfds453543");
+        book2.setPublisher("Tatafdd McGraaw:: ");
+        book2.setLanguage("Engliddsh");
+        book2.setLibrary(library);
+        //bookManagementService.save(book1);
+        List<Book> bookSet = new ArrayList<>();
+        bookSet.add(book1);
+        bookSet.add(book2);
+        library.setBookList(bookSet);
+        return bookSet;
+    }
+
     @Test
     void getAllBooksFromLibrary() throws Exception{
         String uri = "/library/api/getAllBooks/1";
+        when(libraryDataControllerMock.getAllBooksFromLibrary(1l)).thenReturn(bookList());
+
         MvcResult mvcResult = super.mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
 
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
         String content = mvcResult.getResponse().getContentAsString();
-        Library libraryList  = super.mapFromJson(content, Library.class);
+        Book[] libraryList  = super.mapFromJson(content, Book[].class);
+
+        assertNotNull(libraryList);
+        assertEquals(2,libraryList.length);
+        assertEquals("IDGB453543",libraryList[0].getIsbn());
+        assertEquals("ABV",libraryList[0].getAuthor());
+        assertNull(libraryList[0].getTitle());
+
+        assertEquals("IDGBfds453543",libraryList[1].getIsbn());
+        assertEquals("ABdsfdfV",libraryList[1].getAuthor());
+        assertNull(libraryList[1].getTitle());
+
 
 
     }
 
     @Test
     void addBookToLibrary() {
+
+
     }
 
 
